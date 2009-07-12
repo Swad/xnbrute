c0ecd4a82710ffbaf274e9c41b5cb1d7eb6307db
nicknet360 <nicknet360@ba9e3632-6ea4-11de-857f-db15dd68924d>
2009-07-12 07:16:30 +0000

    runtime classes

---
+++ trunk/XnBrute/Data/FighterStatusRandomer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XnBrute.Data
+{
+    class FighterStatusRandomer
+    {
+        /// <summary>
+        /// สุ่ม FighterStatus สำหรับผู้เล่นคนใหม่
+        /// </summary>
+        /// <param name="fighterName"></param>
+        /// <returns></returns>
+        public static FighterStatus Create(string fighterName)
+        {
+            return new FighterStatus();
+        }
+
+        /// <summary>
+        /// ทำการอัพเกรด FighterStatus ของตัวละครที่เลเวลอัพ
+        /// </summary>
+        /// <param name="fighterStatus"></param>
+        public static void LevelUp(FighterStatus fighterStatus)
+        {
+        }
+    }
+}
